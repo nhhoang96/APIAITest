f8e6fb6d6295aa97368554b093dab857d6293a70
Hoang Nguyen <hn1174@messiah.edu>
2016-11-20 23:41:32 -0500

    Push all first

---
+++ app/src/main/java/com/example/hoang/aitest/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.hoang.aitest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.JsonElement;
+
+import java.util.Map;
+
+import ai.api.AIConfiguration;
+import ai.api.AIListener;
+import ai.api.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+
+public class MainActivity extends AppCompatActivity implements AIListener {
+    private Button listenButton;
+    private TextView resultTextView;
+    private AIService aiService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listenButton = (Button) findViewById(R.id.listenButton);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+        final AIConfiguration config = new AIConfiguration("384b243aa7c148b590da67014af0be92",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
+
+        listenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aiService.startListening();
+            }
+        });
+    }
+
+//    public void listenButtonOnClick(final View view) {
+//
+//        aiService.startListening();
+//    }
+    @Override
+
+    public void onResult(final AIResponse response) {
+        Result result = response.getResult();
+
+        // Get parameters
+        String parameterString = "";
+        if (result.getParameters() != null && !result.getParameters().isEmpty()) {
+            for (final Map.Entry<String, JsonElement> entry : result.getParameters().entrySet()) {
+                parameterString += "(" + entry.getKey() + ", " + entry.getValue() + ") ";
+            }
+        } else {
+            Toast.makeText(this, "NOT WORKING", Toast.LENGTH_LONG);
+        }
+
+        // Show results in TextView.
+        resultTextView.setText("Query:" + result.getResolvedQuery() +
+                "\nAction: " + result.getAction() +
+                "\nParameters: " + parameterString);
+    }
+
+    @Override
+    public void onError(AIError error) {
+        resultTextView.setText(error.toString());
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
+    }
+}
